b82f4d5406c5ffa07adf9bd47f248a12b7d6eafd
Igor_U <fixtor75@gmail.com>
2022-08-15 11:25:25 +0300

    Задача на замену символов

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string text = "В Турции уже 2 года как живет мой брат с семьей." +
+                "Они уже обустроились, получили ВНЖ, выбрали район," +
+                "где им понравилось больше всего и даже купили там квартиру." +
+                "Дочка ходит в школу, на кружки, они работают (удаленно). " +
+                "Все налажено, жизнь течёт)";
+
+// string s = "qwerty"
+//             012345
+//             s[3] = r
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text[i] == oldValue)
+            result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
